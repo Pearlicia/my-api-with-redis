9565e8bc412f867be0e4e568cc447398e4c37d4e
Pearlicia <feliciaebikon@gmail.com>
2023-04-21 19:20:39 +0100

    add oauth google

--- app.js
+++ app.js
@@ -6,14 +6,32 @@ require('./helpers/init_mongodb')
 const client = require('./helpers/init_redis')
 const Volcanoe = require("./Models/Volcanoe");
 const { verifyAccessToken } = require('./helpers/jwt_helper')
-
 const AuthRoute = require('./Routes/Auth.route')
+const passport = require('passport');
+const session = require('express-session');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
 
 const app = express()
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+
+// REDIS
 client.SET('test', 'bar')
 client.SET('test1', 'bar1')
 client.SET('test2', 'bar2')
@@ -23,8 +41,53 @@ client.GET('test', (err, value) => {
   console.log(value)
 })
 
-//CREATE
+app.get('/google', function(req, res) {
+  res.render('pages/auth');
+});
 
+var userProfile;
+
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+/*  Google AUTH  */
+ 
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const CALLBACK_URL = process.env.GOAUTHREDIRECT_URL;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/success');
+  });
+
+
+//CREATE NEW VOLCANOE
 app.post("/volcanoes", async (req, res) => {
   const newVolcanoe = new Volcanoe(req.body);
 
@@ -36,7 +99,7 @@ app.post("/volcanoes", async (req, res) => {
   }
 });
 
-//UPDATE
+//UPDATE VOLCANOE
 app.put("/:id", async (req, res) => {
   try {
     const updatedVolcanoe = await Volcanoe.findByIdAndUpdate(
@@ -52,7 +115,7 @@ app.put("/:id", async (req, res) => {
   }
 });
 
-//DELETE
+//DELETE VOLCANOE
 app.delete("/:id",  async (req, res) => {
   try {
     await Volcanoe.findByIdAndDelete(req.params.id);
